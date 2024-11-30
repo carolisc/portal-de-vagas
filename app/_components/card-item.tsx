caeb327e4050d275612c8d4101d75fed16b17568
Carolis <89231697+carolisc@users.noreply.github.com>
2024-11-30 13:17:54 -0300

    feat(card-item): initial styling of job card

--- app/_components/card-item.tsx
+++ app/_components/card-item.tsx
@@ -1,19 +1,35 @@
-import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from './ui/card'
+import Image from 'next/image'
+import { Card, CardHeader, CardTitle, CardDescription } from './ui/card'
+import { Calendar, MapPin, UserRound } from 'lucide-react'
 
 const CardItem = () => {
   return (
     <div className="p-10">
-      <Card>
+      <Card className="h-80 w-56">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <Image
+            className="h-80 w-52 rounded-sm p-1"
+            src="/temos-vagas.jpg"
+            width={200}
+            height={180}
+            alt="Picture of the author"
+          />
+          <CardTitle className="p-1">Cargo</CardTitle>
+          <CardDescription>
+            <div className="flex flex-row">
+              <UserRound />
+              <h2 className="text-sm">Nome e Sobrenome</h2>
+            </div>
+            <div className="flex flex-row pt-1">
+              <MapPin />
+              <p className="text-sm">2.8km</p>
+            </div>
+            <div className="flex flex-row pt-1">
+              <Calendar />
+              <p className="text-sm">3 dias</p>
+            </div>
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
       </Card>
     </div>
   )
