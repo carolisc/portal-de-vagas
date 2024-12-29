eed437c4bbcacdcc46dabaadf6b7a3f436db2904
Carolis <89231697+carolisc@users.noreply.github.com>
2024-12-28 21:48:10 -0300

    feat(data.ts): adding new data: description, company, email, contract

--- app/data.ts
+++ app/data.ts
@@ -4,6 +4,9 @@ export const cardData = [
     title: 'Frontend developer',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Ana Souza',
     distance: '1.5km',
     daysAgo: '2 dias',
@@ -11,6 +14,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Software+Engineer',
     title: 'Engenheiro de Software',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Carlos Almeida',
     distance: '3km',
     daysAgo: '4 dias',
@@ -18,6 +26,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Data+Analyst',
     title: 'Analista de Dados',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Paula Fernandes',
     distance: '5km',
     daysAgo: '1 dia',
@@ -25,6 +38,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=UX%2FUI+Designer',
     title: 'Designer UX/UI',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Rodrigo Ribeiro',
     distance: '2.3km',
     daysAgo: '3 dias',
@@ -32,6 +50,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Project+Manager',
     title: 'Gerente de Projetos',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Fernanda Costa',
     distance: '4.7km',
     daysAgo: '5 dias',
@@ -39,6 +62,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Backend+Developer',
     title: 'Backend developer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Marcos Silva',
     distance: '2km',
     daysAgo: '2 dias',
@@ -46,6 +74,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=QA+Tester',
     title: 'QA Tester',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Juliana Melo',
     distance: '1.8km',
     daysAgo: '3 dias',
@@ -53,6 +86,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Product+Owner',
     title: 'Product Owner',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Gustavo Torres',
     distance: '6km',
     daysAgo: '6 dias',
@@ -60,6 +98,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=Scrum+Master',
     title: 'Scrum Master',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Larissa Rocha',
     distance: '3.5km',
     daysAgo: '2 dias',
@@ -67,6 +110,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=DevOps+Specialist',
     title: 'Especialista DevOps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Renato Oliveira',
     distance: '2.2km',
     daysAgo: '4 dias',
@@ -74,6 +122,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=DevOps+Specialist',
     title: 'Especialista DevOps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Renato Oliveira',
     distance: '2.2km',
     daysAgo: '4 dias',
@@ -81,6 +134,11 @@ export const cardData = [
   {
     image: 'https://via.placeholder.com/200x180.png?text=DevOps+Specialist',
     title: 'Especialista DevOps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    company: 'Empresa X',
+    email: 'email@email.com',
+    contract: 'CLT',
     name: 'Renato Oliveira',
     distance: '2.2km',
     daysAgo: '4 dias',
